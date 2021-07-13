2ea5140976825e1d7ba42efd02ae1e77a7f28d40
Lyndsey Duffield <lyndseyduffield@protonmail.com>
2021-07-13 15:30:07 -0400

    Update button styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -63,6 +63,7 @@ export const Modal: React.FC<Props> = (props: Props) => {
     return (
       <button
         type="button"
+        className="inline-flex w-full justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
         onClick={isLast ? props.handleClose : props.handleNextClick}
       >
         {isLast ? `Finish Tour` : `Next Step`}
